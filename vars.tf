c8b50b8ec7215850352438ed42b112baf70f7482
Raghu K <raghuk.vit@gmail.com>
2023-04-03 08:54:27 +0530

    Update roboshop-jenkins/

--- vars.tf
+++ vars.tf
@@ -1,9 +1,15 @@
 variable "folders" {
-  default = ["Infra"]
+  default = ["Infra", "CI-Pipeline"]
 }
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
   ]
 }
