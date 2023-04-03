8ca7f9453a2240acfbf65046a568571a6c80a89e
Raghu K <raghuk.vit@gmail.com>
2023-04-03 08:16:16 +0530

    Update roboshop-jenkins/

--- vars.tf
+++ vars.tf
@@ -4,6 +4,6 @@ variable "folders" {
 
 variable "jobs" {
   default = [
-    { name = "Infra/roboshop", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
   ]
 }
