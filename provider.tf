7ae65d87caa16df9812e9e5c3f0b2a264b193752
Raghu K <raghuk.vit@gmail.com>
2023-04-03 08:07:32 +0530

    Update roboshop-jenkins/

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
-provider "taiidani/jenkins" {
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
+}
+
+provider "jenkins" {
   server_url = "http://172.31.6.243:8080"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
