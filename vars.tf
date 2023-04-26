1861b93728ef381682e8e018d84af85f1c410f1b
Raghu K <raghuk.vit@gmail.com>
2023-04-26 08:37:27 +0530

    Update roboshop-jenkins/

--- vars.tf
+++ vars.tf
@@ -17,7 +17,7 @@ variable "m-jobs" {
 variable "s-jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra", filename = "Jenkinsfile" },
-    { name = "App-Deployment", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-ansible", filename = "Jenkinsfile-deployment" }
+    { name = "App-Deployment", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-ansible", filename = "Jenkinsfile-deployment" },
     { name = "Helm-Deployment", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-helm-chart", filename = "Jenkinsfile" }
   ]
 }
